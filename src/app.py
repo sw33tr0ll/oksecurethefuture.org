b19327fbd1649023ad4c383fbfd24705182a1bd1
sweetroll <sweetrollbandit@protonmail.com>
2021-10-16 13:49:00 -0500

    it works locally

--- src/app.py
+++ src/app.py
@@ -7,7 +7,8 @@ def donate(event, _):
             <title>OK Secure the Future</title>
         </head>
         <body>
-            <
+            <h3>Coming soon...</h3>
+            <p>Ask Logan for Scholarships</p>
         </body>
     </html>
     """
